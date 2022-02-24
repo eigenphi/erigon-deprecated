3ae5592d7b39a5a3db1fad6337e9e5eb49ee9b64
eval-exec <execvy@gmail.com>
2022-02-24 20:54:26 +0800

    fix multi process wait statement

--- parallel_export_tx.sh
+++ parallel_export_tx.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-set -exu pipefail
+set -u pipefail
 
+RPCDAEMON="/home/zhangheng/eigenphi/erigon/build/bin/rpcdaemon"
 DATA_DIR="/mnt/ssd/eth/erigon_archive_backup"
-OUT_DIR="/mnt/hdd/erigon-export-data"
+OUT_DIR="/mnt/hdd/export-trace-data"
 STEP=10000
 MAX_PROCESS=14
 parallelCount=0
@@ -15,12 +16,13 @@ for i in $(seq 1 ${STEP} 14250000); do
     endBlock=14250000
   fi
 
-  ./rpcdaemon --datadir=${DATA_DIR} export tx ${startBlock} ${endBlock} --out-dir ${OUT_DIR} &>export.tx.log &
+  echo block: ${startBlock} ${endBlock}
+  ${RPCDAEMON} --datadir=${DATA_DIR} export tx ${startBlock} ${endBlock} --out-dir ${OUT_DIR} &>>export.tx.log &
   parallelCount=$((${parallelCount} + 1))
   if [ ${parallelCount} -eq ${MAX_PROCESS} ]; then
-    echo "waiting " ${parallelCount} " jobs"
-    wait -n
+    wait
     parallelCount=0
   fi
 done
-wait -n
+echo "waiting " ${parallelCount} " jobs"
+wait
