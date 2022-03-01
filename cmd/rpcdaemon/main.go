2a67948338c815174bb6b95601402eb96f4960fc
can <can@canx.me>
2022-03-01 16:58:15 +0800

    Add cmd to show build commit
    Usage: "./rpcdaemon version"

--- cmd/rpcdaemon/main.go
+++ cmd/rpcdaemon/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/ledgerwatch/erigon-lib/common"
 	"github.com/ledgerwatch/erigon/cmd/rpcdaemon/cli"
 	"github.com/ledgerwatch/erigon/cmd/rpcdaemon/commands"
+	"github.com/ledgerwatch/erigon/params"
 	"github.com/ledgerwatch/log/v3"
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
@@ -22,6 +24,15 @@ func main() {
 	cmd, cfg := cli.RootCommand()
 	rootCtx, rootCancel := common.RootContext()
 	cmd.AddCommand(commands.GetExportCmd(cfg, rootCancel))
+	cmd.AddCommand(&cobra.Command{
+		Use:   "version",
+		Short: "Print the version of rpcdaemon",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("rpcdaemon", "branch", params.GitBranch,
+				"tag", params.GitTag, "commit", params.GitCommit)
+			os.Exit(0)
+		},
+	})
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
 		ctx := cmd.Context()
 		logger := log.New()
