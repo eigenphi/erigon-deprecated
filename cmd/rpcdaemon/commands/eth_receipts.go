c5b7f49a92343c8e4b479a6f3a56a78d921c4e3e
Eval EXEC <execvy@gmail.com>
2022-06-30 21:42:12 +0800

    apply block receipts api to eth_api

--- cmd/rpcdaemon/commands/eth_receipts.go
+++ cmd/rpcdaemon/commands/eth_receipts.go
@@ -31,6 +31,41 @@ import (
 	"github.com/ledgerwatch/erigon/turbo/transactions"
 )
 
+func getReceipts(ctx context.Context, tx kv.Tx, chainConfig *params.ChainConfig, block *types.Block, senders []common.Address) (types.Receipts, error) {
+	if cached := rawdb.ReadReceipts(tx, block, senders); cached != nil {
+		return cached, nil
+	}
+
+	getHeader := func(hash common.Hash, number uint64) *types.Header {
+		return rawdb.ReadHeader(tx, hash, number)
+	}
+	contractHasTEVM := ethdb.GetHasTEVM(tx)
+	_, _, _, ibs, _, err := transactions.ComputeTxEnv(ctx, block, chainConfig, getHeader, contractHasTEVM, ethash.NewFaker(), tx, block.Hash(), 0)
+	if err != nil {
+		return nil, err
+	}
+
+	usedGas := new(uint64)
+	gp := new(core.GasPool).AddGas(block.GasLimit())
+
+	ethashFaker := ethash.NewFaker()
+	noopWriter := state.NewNoopWriter()
+
+	receipts := make(types.Receipts, len(block.Transactions()))
+
+	for i, txn := range block.Transactions() {
+		ibs.Prepare(txn.Hash(), block.Hash(), i)
+		receipt, _, err := core.ApplyTransaction(chainConfig, getHeader, ethashFaker, nil, gp, ibs, noopWriter, block.Header(), txn, usedGas, vm.Config{}, contractHasTEVM)
+		if err != nil {
+			return nil, err
+		}
+		receipt.BlockHash = block.Hash()
+		receipts[i] = receipt
+	}
+
+	return receipts, nil
+}
+
 func (api *BaseAPI) getReceipts(ctx context.Context, tx kv.Tx, chainConfig *params.ChainConfig, block *types.Block, senders []common.Address) (types.Receipts, error) {
 	if cached := rawdb.ReadReceipts(tx, block, senders); cached != nil {
 		return cached, nil
@@ -450,4 +485,82 @@ Logs:
 		result = append(result, log)
 	}
 	return result
+}
+
+// GetTransactionReceiptsByBlockNumber implements eth_getTransactionReceiptsByBlockNumber. Returns the receipts of transactions given the block number.
+func (api *APIImpl) GetTransactionReceiptsByBlockNumber(ctx context.Context, blockNumber rpc.BlockNumber) ([]map[string]interface{}, error) {
+	tx, err := api.db.BeginRo(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
+
+	// Extract transactions from block
+	block, bErr := api.blockByRPCNumber(blockNumber, tx)
+	if bErr != nil {
+		return nil, bErr
+	}
+	if block == nil {
+		return nil, fmt.Errorf("could not find block  %d", blockNumber.Int64())
+	}
+
+	cc, err := api.chainConfig(tx)
+	if err != nil {
+		return nil, err
+	}
+	txs := block.Transactions()
+
+	receipts, err := getReceipts(ctx, tx, cc, block, block.Body().SendersFromTxs())
+	if err != nil {
+		return nil, fmt.Errorf("getReceipts error: %w", err)
+	}
+	txReceipts := make([]map[string]interface{}, 0, len(txs))
+
+	for idx, receipt := range receipts {
+		tx := txs[idx]
+
+		from, _ := tx.GetSender()
+
+		fields := map[string]interface{}{
+			"blockHash":         block.Hash(),
+			"blockNumber":       hexutil.Uint64(blockNumber),
+			"transactionHash":   tx.Hash(),
+			"transactionIndex":  hexutil.Uint64(idx),
+			"from":              from,
+			"to":                tx.GetTo(),
+			"gasUsed":           hexutil.Uint64(receipt.GasUsed),
+			"cumulativeGasUsed": hexutil.Uint64(receipt.CumulativeGasUsed),
+			"contractAddress":   nil,
+			"logs":              receipt.Logs,
+			"logsBloom":         receipt.Bloom,
+		}
+
+		// Assign receipt status or post state.
+		if len(receipt.PostState) > 0 {
+			fields["root"] = hexutil.Bytes(receipt.PostState)
+		} else {
+			fields["status"] = hexutil.Uint(receipt.Status)
+		}
+		if receipt.Logs == nil {
+			fields["logs"] = [][]*types.Log{}
+		}
+		// If the ContractAddress is 20 0x0 bytes, assume it is not a contract creation
+		if receipt.ContractAddress != (common.Address{}) {
+			fields["contractAddress"] = receipt.ContractAddress
+		}
+
+		txReceipts = append(txReceipts, fields)
+	}
+
+	return txReceipts, nil
+}
+
+func marshalReceipts(receipts types.Receipts, txns []types.Transaction, chainConfig *params.ChainConfig, block *types.Block) []map[string]interface{} {
+	mrs := make([]map[string]interface{}, 0, len(receipts))
+	for i, receipt := range receipts {
+		txn := txns[i]
+		mr := marshalReceipt(receipt, txn, chainConfig, block, txn.Hash())
+		mrs[i] = mr
+	}
+	return mrs
 }
